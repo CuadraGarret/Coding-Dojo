894bf6a5a6703d985ce692ce57cfe137108bab1b
CuadraGarret <cuadragarret@gmail.com>
2023-12-08 21:48:30 -0800

    new file:   Web Fundamentals/Week2/Cuadra_Garret_ButtonClicker.zip 	new file:   Web Fundamentals/Week2/Cuadra_Garret_ButtonClicker/ButtonClicker.html 	new file:   Web Fundamentals/Week2/Cuadra_Garret_ButtonClicker/script.js 	new file:   Web Fundamentals/Week2/Cuadra_Garret_ButtonClicker/style.css

---
+++ Web Fundamentals/Week2/Cuadra_Garret_ButtonClicker/script.js
@@ -0,0 +1,11 @@
+function logout(element) {
+    element.innerText = 'Logout';
+}
+
+function removeFromDOM(element)    {
+    element.remove();
+}
+
+function box(element)   {
+    alert('Ninja was liked');
+}
