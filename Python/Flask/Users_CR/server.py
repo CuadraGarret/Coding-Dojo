e12ff75bfea7baef1da794fd45a47d5a26bed8c6
CuadraGarret <cuadragarret@gmail.com>
2024-02-25 22:37:15 -0800

    modified:   Python/Flask/Users_CR/server.py 	modified:   Python/Flask/Users_CR/templates/read.html 	modified:   Python/Flask/Users_CR/templates/show.html 	new file:   Python/Flask/Users_CR/templates/update.html 	modified:   Python/Flask/Users_CR/user.py 	new file:   Python/Flask/Users_CRUD.zip

--- Python/Flask/Users_CR/server.py
+++ Python/Flask/Users_CR/server.py
@@ -13,6 +13,11 @@ def all_users():
     all_users=User.get_all()
     return render_template("read.html", users=all_users)
 
+@app.route('/users/show/<int:user_id>')
+def show(user_id):
+    user=User.get_one(user_id)
+    return render_template("show.html", user=user)
+
 @app.route("/users/new")
 def new_users():
     return render_template("create.html")
@@ -20,6 +25,21 @@ def new_users():
 @app.route('/users/create', methods=['POST'])
 def create():
     User.save(request.form)
+    return redirect('/users')
+
+@app.route('/users/edit/<int:user_id>')
+def edit(user_id):
+    user=User.get_one(user_id)
+    return render_template('update.html', user=user)
+
+@app.route('/users/update',methods=['POST'])
+def update():
+    User.update(request.form)
+    return redirect('/users')
+
+@app.route('/users/delete/<int:user_id>')
+def delete(user_id):
+    User.delete(user_id)
     return redirect('/users')
 
 if __name__=="__main__":
--- Python/Flask/Users_CR/user.py
+++ Python/Flask/Users_CR/user.py
@@ -31,3 +31,14 @@ class User:
         data = {'id': user_id}
         results = connectToMySQL('users_schema').query_db(query, data)
         return cls(results[0])
+    
+    @classmethod
+    def update(cls,data):
+        query = """UPDATE users SET first_name=%(first_name)s,last_name=%(last_name)s,email=%(email)s WHERE id = %(id)s;"""
+        return connectToMySQL('users_schema').query_db(query,data)
+
+    @classmethod
+    def delete(cls, user_id):
+        query  = "DELETE FROM users WHERE id = %(id)s;"
+        data = {"id": user_id}
+        return connectToMySQL('users_schema').query_db(query, data)
